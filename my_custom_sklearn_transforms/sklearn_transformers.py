f78dd26132535fe18a8f625805bd0f5217f57354
Caroline Moura <54120284+CahMoura@users.noreply.github.com>
2020-08-21 14:28:36 -0300

    Update sklearn_transformers.py

--- my_custom_sklearn_transforms/sklearn_transformers.py
+++ my_custom_sklearn_transforms/sklearn_transformers.py
@@ -2,15 +2,19 @@ from sklearn.base import BaseEstimator, TransformerMixin
 
 
 # All sklearn Transforms must have the `transform` and `fit` methods
-class DropColumns(BaseEstimator, TransformerMixin):
-    def __init__(self, columns):
+class FillNaNColumns(BaseEstimator, TransformerMixin):
+    def __init__(self, columns, fill=0):
         self.columns = columns
-
+        self.fill = fill
+        
     def fit(self, X, y=None):
         return self
 
     def transform(self, X):
         # Primeiro realizamos a cópia do dataframe 'X' de entrada
         data = X.copy()
+        for column in self.columns:
+            data[column].fillna(self.fill,
+                                inplace=True)
         # Retornamos um novo dataframe sem as colunas indesejadas
-        return data.drop(labels=self.columns, axis='columns')
+        return data
